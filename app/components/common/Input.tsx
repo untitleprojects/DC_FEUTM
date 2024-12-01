f9dbff860b171378ec687c0acaf56e6210f700e5
kcjfgnl9205 <kcjfgnl9205@gmail.com>
2024-12-01 17:57:38 +0900

    fix: input 태그 아이콘 추가

--- app/components/common/Input.tsx
+++ app/components/common/Input.tsx
@@ -5,28 +5,37 @@ import React, { useRef, InputHTMLAttributes } from 'react'
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string
   error?: string
+  icon?: React.ReactNode
 }
 
-const Input = ({ label, error, className, ...props }: InputProps) => {
+const Input = ({ label, error, icon, className, ...props }: InputProps) => {
   const inputRef = useRef<HTMLInputElement>(null)
 
   return (
-    <div className={`relative w-full ${className}`}>
-      <input
-        ref={inputRef}
-        placeholder=' '
-        className='pt-3 pb-2 block w-full px-1 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200'
-        {...props}
-      />
-      {label && (
-        <label
-          htmlFor={props.id}
-          className='absolute duration-300 top-2.5 left-0 text-gray-500 -z-10'
-        >
-          {label}
-        </label>
-      )}
-      {error && <p className='text-sm text-red-600 hidden'>{error}</p>}
+    <div>
+      <div
+        className={`relative w-full flex items-center border rounded-md gap-2 px-4 py-2 focus-within:outline focus-within:outline-black ${className} `}
+      >
+        {icon && <div className='text-gray-500 w-6 h-6'>{icon}</div>}
+        <div className='flex-1 relative '>
+          <input
+            ref={inputRef}
+            autoComplete='off'
+            placeholder=' '
+            className='block w-full bg-transparent border-0 appearance-none focus:outline-none focus:ring-0 py-2'
+            {...props}
+          />
+          {label && (
+            <label
+              htmlFor={props.id}
+              className='absolute duration-300 top-2 left-0 text-gray-500 -z-10'
+            >
+              {label}
+            </label>
+          )}
+        </div>
+      </div>
+      {error && <p className='text-sm text-neutral-500 pl-1 pt-1'>{error}</p>}
     </div>
   )
 }
